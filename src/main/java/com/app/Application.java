6719aef9f4245eb36192b0db9f460c3a2fd6c1b6
Dev3 <Dev3@gmail.com>
2020-07-21 15:50:05 +0530

    Main class modified

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+//changed here********************8
+
 		SpringApplication.run(Application.class, args);
 	}
 
